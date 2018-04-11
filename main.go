5a02055f0f84e8357f3dd18cfd457e3c99f37a6a
康  查 <956468883@qq.com>
2018-04-11 22:27:49 +0800

    修改

--- main.go
+++ main.go
@@ -1,37 +1,80 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
+
+	_ "github.com/lib/pq"
+)
+
+var schema = `
+CREATE TABLE person (
+    first_name text,
+    last_name text,
+    email text
+);
+
+CREATE TABLE place (
+    country text,
+    city text NULL,
+    telcode integer
+)`
+
+// type Person struct {
+// 	FirstName string `db:"first_name"`
+// 	LastName  string `db:"last_name"`
+// 	Email     string
+// }
+
+// type Place struct {
+// 	Country string
+// 	City    sql.NullString
+// 	TelCode int
+// }
 
 func main() {
 	app := iris.New()
-	// Load all templates from the "./views" folder
-	// where extension is ".html" and parse them
-	// using the standard `html/template` package.
-	app.RegisterView(iris.HTML("./views", ".html"))
+	app.Logger().SetLevel("debug")
+	// Optionally, add two built'n handlers
+	// that can recover from any http-relative panics
+	// and log the requests to the terminal.
+	app.Use(recover.New())
+	app.Use(logger.New())
 
-	// Method:    GET
-	// Resource:  http://localhost:8080
-	app.Get("/", func(ctx iris.Context) {
-		// Bind: {{.message}} with "Hello world!"
-		ctx.ViewData("message", "Hello world!")
-		// Render template file: ./views/hello.html
-		ctx.View("hello.html")
+	// Method:   GET
+	// Resource: http://localhost:8080
+	app.Handle("GET", "/", func(ctx iris.Context) {
+		ctx.HTML("<h1>Welcome</h1>")
 	})
 
-	// Method:    GET
-	// Resource:  http://localhost:8080/user/42
-	//
-	// Need to use a custom regexp instead?
-	// Easy,
-	// just mark the parameter's type to 'string'
-	// which accepts anything and make use of
-	// its `regexp` macro function, i.e:
-	// app.Get("/user/{id:string regexp(^[0-9]+$)}")
-	app.Get("/user/{id:long}", func(ctx iris.Context) {
-		userID, _ := ctx.Params().GetInt64("id")
-		ctx.Writef("User ID: %d", userID)
+	// same as app.Handle("GET", "/ping", [...])
+	// Method:   GET
+	// Resource: http://localhost:8080/ping
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.WriteString("pong")
 	})
 
-	// Start the server using a network address.
-	app.Run(iris.Addr(":8080"))
+	// Method:   GET
+	// Resource: http://localhost:8080/hello
+	app.Get("/hello", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "Hello Iris!"})
+	})
+
+	app.Get("/initDb", func(ctx iris.Context) {
+		db, err := sqlx.Connect("postgres", "user=kangcha dbname=mydb sslmode=disable password=123456")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		db.MustExec(schema)
+		ctx.Writef("ok %d")
+	})
+
+	// http://localhost:8080
+	// http://localhost:8080/ping
+	// http://localhost:8080/hello
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
