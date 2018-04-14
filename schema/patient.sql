24fc0de3df87e56dcaaad93239bdc8b9634d937d
康  查 <956468883@qq.com>
2018-04-14 13:03:01 +0800

    修改

--- schema/patient.sql
+++ schema/patient.sql
@@ -1,15 +1,15 @@
 --就诊人
 CREATE TABLE patient
 (
-  cert_no varchar(18) PRIMARY KEY NOT NULL,
-  name varchar(10) NOT NULL,
-  birthday varchar(8) NOT NULL,
-  sex integer NOT NULL CHECK(sex = 0 OR sex = 1),
-  phone varchar(11) not Null,
-  address varchar(40),
-  profession varchar(40),
-  remark varchar(200),
-  status boolean NOT NULL DEFAULT true,
+  cert_no varchar(18) PRIMARY KEY NOT NULL, --身份证号
+  name varchar(10) NOT NULL,--姓名
+  birthday varchar(8) NOT NULL,--身份证号
+  sex integer NOT NULL CHECK(sex = 0 OR sex = 1),--性别 0：女，1：男
+  phone varchar(11) not Null,--手机号
+  address varchar(40),--住址
+  profession varchar(40),--职业
+  remark varchar(200),--备注
+  status boolean NOT NULL DEFAULT true,--是否启用
   created_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   deleted_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP
