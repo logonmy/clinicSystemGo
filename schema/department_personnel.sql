99ff6953549a46a1a02e9daabd787c12dbbae45b
康  查 <956468883@qq.com>
2018-04-14 15:23:15 +0800

    修改

--- schema/department_personnel.sql
+++ schema/department_personnel.sql
@@ -1,13 +1,12 @@
 --人员科室关系表
 CREATE TABLE department_personnel
 (
-  id serial PRIMARY KEY NOT NULL,--id
-  department_id INTEGER NOT NULL references department(id),--人员编码
-  personnel_id INTEGER NOT NULL references personnel(id),--诊所编码
+  department_id INTEGER NOT NULL references department(id),--科室id
+  personnel_id INTEGER NOT NULL references personnel(id),--人员id
   type INTEGER NOT NULL CHECK(type > 0 AND type < 3),-- 关系类型 1：人事科室， 2：出诊科室
   status boolean NOT NULL DEFAULT true,--是否启用
   created_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   deleted_time timestamp,
-  UNIQUE(department_id, personnel_id, type)--联合主键，人员id、科室id、 类别唯一
+  PRIMARY KEY(department_id, personnel_id, type)--联合主键，人员id、科室id、 类别唯一
 );
--- schema/personnel_role.sql
+++ schema/personnel_role.sql
@@ -1,13 +1,12 @@
 --用户角色表
 CREATE TABLE role
 (
-  personnel_code varchar(10) NOT NULL references personnel(code),--人员编码
-  clinic_code varchar(40) NOT NULL references clinic(code),--诊所编码
+  personnel_id INTEGER NOT NULL references personnel(id),--人员id
   role_id INTEGER NOT NULL references role(id),--角色id
   status boolean NOT NULL DEFAULT true,--是否启用
   created_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   deleted_time timestamp,
-  PRIMARY KEY (personnel_code, clinic_code, role_id)--联合主键，人员编码、诊所编码、 角色编码唯一
+  PRIMARY KEY (personnel_id, role_id)--联合主键，人员编码、角色编码唯一
 );
 
