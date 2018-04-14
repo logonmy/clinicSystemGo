50c6139bf40c241e733f9e1554f874f6b10fe44e
康  查 <956468883@qq.com>
2018-04-14 13:11:31 +0800

    修改

--- schema/admin.sql
+++ schema/admin.sql
@@ -1,21 +1,14 @@
 --诊所管理员
 CREATE TABLE admin
 (
-  username varchar(20) NOT NULL,
-  --用户名
-  phone varchar(11) NOT NULL,
-  --手机号
-  email VARCHAR(30),
-  --邮箱
-  password varchar(40) NOT NULL,
-  --密码
-  clinic_code varchar(40) NOT NULL references clinic(code),
-  --诊所编码
-  status boolean NOT NULL DEFAULT true,
-  --是否启用
+  username varchar(20) NOT NULL,--用户名
+  phone varchar(11) NOT NULL,--手机号
+  email VARCHAR(30),--邮箱
+  password varchar(40) NOT NULL,--密码
+  clinic_code varchar(40) NOT NULL references clinic(code),--诊所编码
+  status boolean NOT NULL DEFAULT true,--是否启用
   created_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   deleted_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
-  PRIMARY KEY (username, clinic_code)
-  --联合主键，用户名和诊所编码唯一
+  PRIMARY KEY (username, clinic_code)--联合主键，用户名和诊所编码唯一
 );
