432cd924434e2562a4e6c8b0adcf98509b291b59
田先耀 <xianyao9958@163.com>
2018-04-14 14:26:13 +0800

    新增人员表 科室表

---
+++ schema/department.sql
@@ -0,0 +1,14 @@
+--科室
+CREATE TABLE department
+(
+	code varchar(20) NOT NULL,--科室编码
+	name varchar(20) NOT NULL,--科室名称
+	clinic_code varchar(40) NOT NULL references clinic(code),--所属诊所
+	status boolean NOT NULL DEFAULT true,--是否启用
+	is_appointment boolean NOT NULL,--是否开放预约/挂号
+	is_personnel boolean NOT NULL,--是否人事科室
+	created_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
+	updated_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
+	deleted_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
+	PRIMARY KEY (dept_code, clinic_code)
+);
