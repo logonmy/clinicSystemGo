ff33b099b0dbfec3c14cbe87990fed0bf4023f41
田先耀 <xianyao9958@163.com>
2018-04-14 14:50:47 +0800

    修改人员表

--- schema/personnel.sql
+++ schema/personnel.sql
@@ -7,11 +7,11 @@ CREATE TABLE personnel
   dept_code varchar(20) NOT NULL references department(code),--所属科室
   weight integer NOT NULL DEFAULT 1,--权重
   title varchar(10) NOT NULL,--职称
-  account varchar(20) NOT NULL,--账号
+  username varchar(20) NOT NULL,--账号
   password varchar(40) NOT NULL,--密码
   status boolean NOT NULL DEFAULT true,--是否启用
   created_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
   deleted_time timestamp NOT NULL DEFAULT LOCALTIMESTAMP,
-  PRIMARY KEY (dept_code, clinic_code)
+  PRIMARY KEY (code, clinic_code)
 );
