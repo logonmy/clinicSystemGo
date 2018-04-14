8c3d3a142c392f5f037a2468e2678dde298fc72a
ruan <894159898@qq.com>
2018-04-14 17:10:56 +0800

    预约记录

--- schema/appointment.sql
+++ schema/appointment.sql
@@ -1,18 +1,19 @@
---医生出诊排班
-CREATE TABLE doctor_visit_schedule
+--挂号记录
+CREATE TABLE appointment
 (
-  id integer PRIMARY KEY NOT NULL, --排班编号
+  id serial PRIMARY KEY NOT NULL, --编号
+  clinic_patient_id integer NOT NULL references clinic_patient(id),--患者id
   department_id VARCHAR(10) REFERENCES department_personnel(department_id),--医生编码
   personnel_id VARCHAR(10) REFERENCES department_personnel(personnel_id),--科室编码
   visit_date DATA NOT NULL,--出诊日期
   am_pm CHAR(1) NOT NULL CHECK(am_pm = 'a' OR am_pm = 'p'),--出诊上下午
-  stop_flag boolean NOT NULL DEFAULT false,--停诊标识
   is_today boolean NOT NULL DEFAULT false,--是否当日号
-  tatal_num INT NOT NULL DEFAULT 20,--总的接诊数
-  left_num INT NOT NULL DEFAULT 20 CHECK(left_num <= tatal_num),--剩余接诊数
   visit_type_code integer NOT NULL REFERENCES visit_type(code),--出诊类型编码
+  status VARCHAR(2) NOT NULL DEFAULT '01',--就诊状态
+  visit_place VARCHAR(20),--就诊未知
+  sort_no SMALLINT,--就诊序号
+  operationCode VARCHAR(10),--操作人编码
   created_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
-  deleted_time TIMESTAMP,
-  UNIQUE (doctor_code,dept_code,visit_date,am_pm,visit_code,is_today)
+  deleted_time TIMESTAMP
 );
