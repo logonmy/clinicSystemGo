1bd64ff02d126e86405b115f02da6f6c1046543f
田先耀 <xianyao9958@163.com>
2018-04-14 15:25:01 +0800

    修改

--- schema/department.sql
+++ schema/department.sql
@@ -4,7 +4,7 @@ CREATE TABLE department
 	id serial PRIMARY KEY NOT NULL,--id
 	code varchar(20) NOT NULL,--科室编码
 	name varchar(20) NOT NULL,--科室名称
-	clinic_code varchar(40) NOT NULL references clinic(code),--所属诊所
+	clinic_code varchar(20) NOT NULL references clinic(code),--所属诊所
 	status boolean NOT NULL DEFAULT true,--是否启用
 	is_appointment boolean NOT NULL,--是否开放预约/挂号
 	is_personnel boolean NOT NULL,--是否人事科室
