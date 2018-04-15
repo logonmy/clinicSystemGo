9e7899c70eebc33b4dc813b9446f83eda76764fd
田先耀 <xianyao9958@163.com>
2018-04-15 15:05:41 +0800

    修改

--- schema/visit_type.sql
+++ schema/visit_type.sql
@@ -4,7 +4,7 @@ CREATE TABLE visit_type
   code integer PRIMARY KEY NOT NULL,--编码
   name VARCHAR(10) NOT NULL,--出诊类型名称
   open_flag boolean NOT NULL DEFAULT true,--启用状态
-  fee money NOT NULL DEFAULT 0.00,
+  fee integer NOT NULL DEFAULT 0 CHECK(fee > 0),
   created_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
   deleted_time TIMESTAMP
