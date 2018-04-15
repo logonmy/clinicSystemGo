44ea223326a616a8b7a6f5791104b253f95099ba
康  查 <956468883@qq.com>
2018-04-15 15:04:14 +0800

    修改

--- schema/doctor_visit_schedule_modle.sql
+++ schema/doctor_visit_schedule_modle.sql
@@ -6,7 +6,7 @@ CREATE TABLE doctor_visit_schedule_modle
   personnel_id integer REFERENCES personnel(id),--科室id
   weekday INTEGER NOT NULL CHECK(weekday BETWEEN -1 AND 7),--出诊 日期（周几，0 代表 周日，1 周一...）
   am_pm CHAR(1) NOT NULL CHECK(am_pm = 'a' OR am_pm = 'p'),--出诊上下午
-  tatal_num INT NOT NULL DEFAULT 20,--总的接诊数
+  tatal_num integer NOT NULL DEFAULT 20,--总的接诊数
   visit_type_code integer NOT NULL REFERENCES visit_type(code),--出诊类别编码
   created_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
