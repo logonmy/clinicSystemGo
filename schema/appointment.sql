c1d9bb729724a3bdfd3f45fefbf82f7f4e53ec50
康  查 <956468883@qq.com>
2018-04-15 14:41:08 +0800

    修改

--- schema/appointment.sql
+++ schema/appointment.sql
@@ -3,8 +3,8 @@ CREATE TABLE appointment
 (
   id serial PRIMARY KEY NOT NULL, --编号
   clinic_patient_id integer NOT NULL references clinic_patient(id),--患者id
-  department_id VARCHAR(10) REFERENCES department_personnel(department_id),--医生编码
-  personnel_id VARCHAR(10) REFERENCES department_personnel(personnel_id),--科室编码
+  department_id VARCHAR(10) NOT NULL REFERENCES department(id),--医生编码
+  personnel_id VARCHAR(10) NOT NULL REFERENCES personnel(id),--科室编码
   visit_date DATA NOT NULL,--出诊日期
   am_pm CHAR(1) NOT NULL CHECK(am_pm = 'a' OR am_pm = 'p'),--出诊上下午
   is_today boolean NOT NULL DEFAULT false,--是否当日号
@@ -12,7 +12,7 @@ CREATE TABLE appointment
   status VARCHAR(2) NOT NULL DEFAULT '01',--就诊状态
   visit_place VARCHAR(20),--就诊未知
   sort_no SMALLINT,--就诊序号
-  operationCode VARCHAR(10),--操作人编码
+  operation_id integer REFERENCES personnel(id),--操作人编码
   created_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
   deleted_time TIMESTAMP
