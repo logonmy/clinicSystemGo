ca4896a9f35808b21197c4465dd663bcbd0bb851
田先耀 <xianyao9958@163.com>
2018-04-16 22:27:19 +0800

    科室修改

--- controller/department.go
+++ controller/department.go
@@ -19,13 +19,53 @@ func DepartmentCreate(ctx iris.Context) {
 		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
 		return
 	}
-	stmt, err := model.DB.Prepare("INSERT INTO department (code, name, clinic_code, weight) VALUES ($1, $2, $3, $4) RETURNING id")
+	var departmentID int
+	err := model.DB.QueryRow("INSERT INTO department (code, name, clinic_code, weight) VALUES ($1, $2, $3, $4) RETURNING id", code, name, clinicCode, weight).Scan(&departmentID)
 	if err != nil {
 		fmt.Println("Perr ===", err)
 		ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
 		return
 	}
-	res, err := stmt.Exec(code, name, clinicCode, weight)
+	ctx.JSON(iris.Map{"code": "200", "data": departmentID})
+}
+
+/**
+ * 获取科室
+ */
+func DepartmentList(ctx iris.Context) {
+	keyword := ctx.PostValue("keyword")
+	clinicCode := ctx.PostValue("clinicCode")
+	if clinicCode == "" {
+		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
+		return
+	}
+	var results []map[string]interface{}
+	if keyword != "" {
+		rows, _ := model.DB.Queryx("SELECT * FROM department WHERE (code=$1 OR (name LIKE '%' || $1 || '%')) AND clinic_code=$2", keyword, clinicCode)
+		results = FormatSQLRowsToMapArray(rows)
+	} else {
+		rows, _ := model.DB.Queryx("SELECT * FROM department WHERE clinic_code=$1", clinicCode)
+		results = FormatSQLRowsToMapArray(rows)
+	}
+	ctx.JSON(iris.Map{"code": "200", "data": results})
+}
+
+/**
+ * 删除科室
+ */
+func DepartmentDelete(ctx iris.Context) {
+	departmentID := ctx.PostValue("departmentID")
+	if departmentID == "" {
+		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
+		return
+	}
+	stmt, err := model.DB.Prepare("DELETE from department WHERE id=$1")
+	if err != nil {
+		fmt.Println("Perr ===", err)
+		ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
+		return
+	}
+	res, err := stmt.Exec(departmentID)
 	if err != nil {
 		fmt.Println("Eerr ===", err)
 		ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
@@ -35,31 +75,28 @@ func DepartmentCreate(ctx iris.Context) {
 }
 
 /**
- * 获取科室
+ * 修改科室
  */
-func DepartmentGet(ctx iris.Context) {
-	keyword := ctx.PostValue("keyword")
+func DepartmentUpdate(ctx iris.Context) {
+	code := ctx.PostValue("code")
+	name := ctx.PostValue("name")
 	clinicCode := ctx.PostValue("clinicCode")
-	if clinicCode == "" {
+	weight := ctx.PostValue("weight")
+	if code == "" || name == "" || clinicCode == "" || weight == "" {
 		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
 		return
 	}
-	var sql string
-	departments := []model.Department{}
-	if keyword != "" {
-		sql = "SELECT * FROM department WHERE (code=$1 OR (name LIKE '%' || $1 || '%')) AND clinic_code=$2"
-		err := model.DB.Select(&departments, sql, keyword, clinicCode)
-		if err != nil {
-			fmt.Println("err ===", err)
-			ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
-		}
-	} else {
-		sql = "SELECT * FROM department WHERE clinic_code=$1"
-		err := model.DB.Select(&departments, sql, clinicCode)
-		if err != nil {
-			fmt.Println("err ===", err)
-			ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
-		}
+	stmt, err := model.DB.Prepare("UPDATE department SET ")
+	if err != nil {
+		fmt.Println("Perr ===", err)
+		ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
+		return
 	}
-	ctx.JSON(iris.Map{"code": "200", "data": departments})
+	res, err := stmt.Exec(departmentID)
+	if err != nil {
+		fmt.Println("Eerr ===", err)
+		ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
+		return
+	}
+	ctx.JSON(iris.Map{"code": "200", "data": res})
 }
