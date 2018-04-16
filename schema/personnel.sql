01ad994a25a3b3fbab8cc2b240c5026dfa08360b
康  查 <956468883@qq.com>
2018-04-16 15:28:53 +0800

    修改

--- schema/personnel.sql
+++ schema/personnel.sql
@@ -7,7 +7,7 @@ CREATE TABLE personnel
   clinic_code varchar(20) NOT NULL references clinic(code),--所属诊所
   weight integer NOT NULL DEFAULT 1,--权重
   title varchar(10),--职称
-  username varchar(20),--账号
+  username varchar(20) UNIQUE,--账号
   password varchar(40),--密码
   is_clinic_admin boolean NOT NULL DEFAULT false,--是否是诊所超级管理员
   status boolean NOT NULL DEFAULT true,--是否启用
