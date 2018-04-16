336be95beed2dfe652fe157a7125c5e5c46fb178
田先耀 <xianyao9958@163.com>
2018-04-16 22:58:22 +0800

    修改

--- controller/department.go
+++ controller/department.go
@@ -16,50 +16,87 @@ func DepartmentCreate(ctx iris.Context) {
 	clinicCode := ctx.PostValue("clinicCode")
 	weight := ctx.PostValue("weight")
 	if code == "" || name == "" || clinicCode == "" || weight == "" {
-		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
+		ctx.JSON(iris.Map{"code": "1", "msg": "缺少参数"})
 		return
 	}
-	stmt, err := model.DB.Prepare("INSERT INTO department (code, name, clinic_code, weight) VALUES ($1, $2, $3, $4) RETURNING id")
+	var departmentID int
+	err := model.DB.QueryRow("INSERT INTO department (code, name, clinic_code, weight) VALUES ($1, $2, $3, $4) RETURNING id", code, name, clinicCode, weight).Scan(&departmentID)
 	if err != nil {
-		fmt.Println("Perr ===", err)
-		ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
+		fmt.Println("err ===", err)
+		ctx.JSON(iris.Map{"code": "1", "msg": "error"})
 		return
 	}
-	res, err := stmt.Exec(code, name, clinicCode, weight)
-	if err != nil {
-		fmt.Println("Eerr ===", err)
-		ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
-		return
-	}
-	ctx.JSON(iris.Map{"code": "200", "data": res})
+	ctx.JSON(iris.Map{"code": "200", "data": departmentID})
 }
 
 /**
  * 获取科室
  */
-func DepartmentGet(ctx iris.Context) {
+func DepartmentList(ctx iris.Context) {
 	keyword := ctx.PostValue("keyword")
 	clinicCode := ctx.PostValue("clinicCode")
 	if clinicCode == "" {
-		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
+		ctx.JSON(iris.Map{"code": "1", "msg": "缺少参数"})
 		return
 	}
-	var sql string
-	departments := []model.Department{}
+	var results []map[string]interface{}
 	if keyword != "" {
-		sql = "SELECT * FROM department WHERE (code=$1 OR (name LIKE '%' || $1 || '%')) AND clinic_code=$2"
-		err := model.DB.Select(&departments, sql, keyword, clinicCode)
-		if err != nil {
-			fmt.Println("err ===", err)
-			ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
-		}
+		rows, _ := model.DB.Queryx("SELECT * FROM department WHERE (code=$1 OR (name LIKE '%' || $1 || '%')) AND clinic_code=$2", keyword, clinicCode)
+		results = FormatSQLRowsToMapArray(rows)
 	} else {
-		sql = "SELECT * FROM department WHERE clinic_code=$1"
-		err := model.DB.Select(&departments, sql, clinicCode)
-		if err != nil {
-			fmt.Println("err ===", err)
-			ctx.JSON(iris.Map{"code": "-1", "msg": "error"})
-		}
+		rows, _ := model.DB.Queryx("SELECT * FROM department WHERE clinic_code=$1", clinicCode)
+		results = FormatSQLRowsToMapArray(rows)
 	}
-	ctx.JSON(iris.Map{"code": "200", "data": departments})
+	ctx.JSON(iris.Map{"code": "200", "data": results})
+}
+
+/**
+ * 删除科室
+ */
+func DepartmentDelete(ctx iris.Context) {
+	departmentID := ctx.PostValue("departmentID")
+	if departmentID == "" {
+		ctx.JSON(iris.Map{"code": "1", "msg": "缺少参数"})
+		return
+	}
+	stmt, err := model.DB.Prepare("DELETE from department WHERE id=$1")
+	if err != nil {
+		fmt.Println("Perr ===", err)
+		ctx.JSON(iris.Map{"code": "1", "msg": "error"})
+		return
+	}
+	res, err := stmt.Exec(departmentID)
+	if err != nil {
+		fmt.Println("Eerr ===", err)
+		ctx.JSON(iris.Map{"code": "1", "msg": "error"})
+	}
+	ctx.JSON(iris.Map{"code": "200", "data": res})
+}
+
+/**
+ * 编辑科室
+ */
+func DepartmentUpdate(ctx iris.Context) {
+	departmentID := ctx.PostValue("departmentID")
+	code := ctx.PostValue("code")
+	name := ctx.PostValue("name")
+	clinicCode := ctx.PostValue("clinicCode")
+	weight := ctx.PostValue("weight")
+	if departmentID == "" || code == "" || name == "" || clinicCode == "" || weight == "" {
+		ctx.JSON(iris.Map{"code": "1", "msg": "缺少参数"})
+		return
+	}
+	stmt, err := model.DB.Prepare("UPDATE department SET code=$2, name=$3, clinic_code=$4, weight=$5 WHERE id=$1")
+	if err != nil {
+		fmt.Println("Perr ===", err)
+		ctx.JSON(iris.Map{"code": "1", "msg": "error"})
+		return
+	}
+	res, err := stmt.Exec(departmentID, code, name, clinicCode, weight)
+	if err != nil {
+		fmt.Println("Eerr ===", err)
+		ctx.JSON(iris.Map{"code": "1", "msg": "error"})
+		return
+	}
+	ctx.JSON(iris.Map{"code": "200", "data": res})
 }
