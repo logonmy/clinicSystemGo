dd3ecc1b9a2719f24907fe1f9138faa7993c8d0b
田先耀 <xianyao9958@163.com>
2018-04-22 22:30:23 +0800

    修改

--- schema/init.sql
+++ schema/init.sql
@@ -177,24 +177,6 @@ CREATE TABLE clinic_triage_patient
   UNIQUE (department_id, clinic_patient_id,treat_status,visit_date)--联合主键，就诊人、科室、状态、日期唯一
 );
 
---预约记录
-CREATE TABLE appointment
-(
-  id serial PRIMARY KEY NOT NULL, --编号
-  clinic_patient_id integer NOT NULL references clinic_patient(id),--患者id
-  department_id integer NOT NULL REFERENCES department(id),--医生编码
-  personnel_id integer NOT NULL REFERENCES personnel(id),--科室编码
-  visit_date DATE NOT NULL,--出诊日期
-  am_pm varchar(1) NOT NULL CHECK(am_pm = 'a' OR am_pm = 'p'),--出诊上下午
-  is_today boolean NOT NULL DEFAULT false,--是否当日号
-  visit_type_code integer NOT NULL REFERENCES visit_type(code),--出诊类型编码
-  visit_place VARCHAR(20),--就诊地址
-  operation_id integer REFERENCES personnel(id),--操作人编码
-  created_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
-  updated_time TIMESTAMP NOT NULL DEFAULT LOCALTIMESTAMP,
-  deleted_time TIMESTAMP
-);
-
 --挂号记录
 CREATE TABLE registration
 (
