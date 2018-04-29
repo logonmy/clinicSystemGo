00030cc0ef3b9e02a6c097889f10f9b98fd20a41
康  查 <956468883@qq.com>
2018-04-29 18:12:43 +0800

    xiugai

--- schema/init.sql
+++ schema/init.sql
@@ -126,11 +126,11 @@ CREATE TABLE patient_channel
 CREATE TABLE patient
 (
   id serial PRIMARY KEY NOT NULL,--id
-  cert_no varchar(18) UNIQUE NOT NULL,--身份证号
+  cert_no varchar(18) UNIQUE,--身份证号
   name varchar(10) NOT NULL,--姓名
   birthday varchar(8) NOT NULL,--身份证号
   sex integer NOT NULL CHECK(sex = 0 OR sex = 1),--性别 0：女，1：男
-  phone varchar(11) not Null,--手机号
+  phone varchar(11) not NULL,--手机号
   patient_channel_id INTEGER NOT Null references patient_channel(id),
   address varchar(40),--住址
   profession varchar(40),--职业
@@ -138,7 +138,8 @@ CREATE TABLE patient
   status boolean NOT NULL DEFAULT true,--是否启用
   created_time timestamp with time zone NOT NULL DEFAULT LOCALTIMESTAMP,
   updated_time timestamp with time zone NOT NULL DEFAULT LOCALTIMESTAMP,
-  deleted_time timestamp with time zone
+  deleted_time timestamp with time zone,
+  UNIQUE (name, birthday, phone)--唯一
 );
 
 --诊所就诊人
