f7efd191781310f27b80efd00593a26cf320b843
康  查 <956468883@qq.com>
2018-05-12 15:52:45 +0800

    xiugai

--- controller/triage.go
+++ controller/triage.go
@@ -4,6 +4,7 @@ import (
 	"clinicSystemGo/model"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
 	"time"
@@ -1516,4 +1517,252 @@ func PrescriptionWesternPatientCreate(ctx iris.Context) {
 	}
 
 	ctx.JSON(iris.Map{"code": "200", "data": nil})
+}
+
+//TreatmentPatientGet 查询治疗
+func TreatmentPatientGet(ctx iris.Context) {
+	clinicTriagePatientID := ctx.PostValue("clinic_triage_patient_id")
+	if clinicTriagePatientID == "" {
+		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
+		return
+	}
+
+	rows, err := model.DB.Queryx(`select tp.*, t.name, du.name as unit_name from treatment_patient tp left join clinic_treatment ct on tp.clinic_treatment_id = ct.id 
+		left join treatment t on ct.treatment_id = t.id
+		left join dose_unit du on t.unit_id = du.id
+		where tp.clinic_triage_patient_id = $1`, clinicTriagePatientID)
+	if err != nil {
+		ctx.JSON(iris.Map{"code": "-1", "msg": err.Error()})
+	}
+
+	result := FormatSQLRowsToMapArray(rows)
+
+	ctx.JSON(iris.Map{"code": "200", "data": result})
+}
+
+//LaboratoryPatientGet 获取检验
+func LaboratoryPatientGet(ctx iris.Context) {
+	clinicTriagePatientID := ctx.PostValue("clinic_triage_patient_id")
+	if clinicTriagePatientID == "" {
+		ctx.JSON(iris.Map{"code": "1", "msg": "缺少参数"})
+		return
+	}
+
+	rows, err := model.DB.Queryx(`select lp.*, l.name from laboratory_patient lp left join clinic_laboratory cl on lp.clinic_laboratory_id = cl.id 
+		join laboratory l on cl.laboratory_id = l.id
+		where lp.clinic_triage_patient_id = $1`, clinicTriagePatientID)
+
+	if err != nil {
+		ctx.JSON(iris.Map{"code": "-1", "msg": err.Error()})
+	}
+
+	result := FormatSQLRowsToMapArray(rows)
+	ctx.JSON(iris.Map{"code": "200", "data": result})
+}
+
+//ExaminationPatientCreate 开检查
+func ExaminationPatientCreate(ctx iris.Context) {
+	clinicTriagePatientID := ctx.PostValue("clinic_triage_patient_id")
+	personnelID := ctx.PostValue("personnel_id")
+	items := ctx.PostValue("items")
+
+	if clinicTriagePatientID == "" {
+		ctx.JSON(iris.Map{"code": "1", "msg": "缺少参数"})
+		return
+	}
+	if items == "" {
+		ctx.JSON(iris.Map{"code": "200", "data": nil})
+		return
+	}
+
+	var results []map[string]string
+	errj := json.Unmarshal([]byte(items), &results)
+	fmt.Println("results===", results)
+	if errj != nil {
+		ctx.JSON(iris.Map{"code": "-1", "msg": errj.Error()})
+		return
+	}
+	row := model.DB.QueryRowx(`select id,status from clinic_triage_patient where id=$1 limit 1`, clinicTriagePatientID)
+	if row == nil {
+		ctx.JSON(iris.Map{"code": "1", "msg": "保存检查失败,分诊记录错误"})
+		return
+	}
+
+	prow := model.DB.QueryRowx("select id from personnel where id=$1 limit 1", personnelID)
+	if prow == nil {
+		ctx.JSON(iris.Map{"code": "1", "msg": "保存检查失败,操作员错误"})
+		return
+	}
+	clinicTriagePatient := FormatSQLRowToMap(row)
+	personnel := FormatSQLRowToMap(prow)
+
+	_, ok := clinicTriagePatient["id"]
+	if !ok {
+		ctx.JSON(iris.Map{"code": "1", "msg": "分诊记录不存在"})
+		return
+	}
+	status := clinicTriagePatient["status"]
+	if status.(int64) != 30 {
+		ctx.JSON(iris.Map{"code": "1", "msg": "分诊记录当前状态错误"})
+		return
+	}
+	_, pok := personnel["id"]
+	if !pok {
+		ctx.JSON(iris.Map{"code": "1", "msg": "操作员错误"})
+		return
+	}
+
+	var mzUnpaidOrdersValues []string
+	mzUnpaidOrdersSets := []string{
+		"clinic_triage_patient_id",
+		"charge_project_type_id",
+		"charge_project_id",
+		"order_sn",
+		"soft_sn",
+		"name",
+		"price",
+		"amount",
+		"unit",
+		"total",
+		"fee",
+		"operation_id",
+	}
+
+	var clinicExaminationValues []string
+	clinicExaminationSets := []string{
+		"clinic_triage_patient_id",
+		"clinic_examination_id",
+		"order_sn",
+		"soft_sn",
+		"times",
+		"organ",
+		"operation_id",
+		"illustration",
+	}
+
+	orderSn := FormatPayOrderSn(clinicTriagePatientID, "4")
+
+	for index, v := range results {
+		clinicExaminationID := v["clinic_examination_id"]
+		times := v["times"]
+		illustration := v["illustration"]
+		organ := v["organ"]
+		fmt.Println("clinicExaminationID====", clinicExaminationID)
+		var sl []string
+		var sm []string
+		examinationSQL := `select ce.id as clinic_examination_id,ce.price,ce.is_discount,e.name,du.name as dose_unit_name from clinic_examination ce
+		left join examination e on e.id = ce.examination_id
+		left join dose_unit du on du.id = e.unit_id
+		where ce.id=$1`
+		trow := model.DB.QueryRowx(examinationSQL, clinicExaminationID)
+		if trow == nil {
+			ctx.JSON(iris.Map{"code": "1", "msg": "检查项错误"})
+			return
+		}
+		examination := FormatSQLRowToMap(trow)
+		fmt.Println("====", examination)
+		_, ok := examination["clinic_examination_id"]
+		if !ok {
+			ctx.JSON(iris.Map{"code": "1", "msg": "选择的检查项错误"})
+			return
+		}
+		timestamp := time.Now().Unix()
+		random := strconv.FormatFloat(rand.Float64(), 'E', 10, 64)[2:5]
+		price := examination["price"].(int64)
+		name := examination["name"].(string)
+		unitName := examination["dose_unit_name"].(string)
+		amount, _ := strconv.Atoi(times)
+		total := int(price) * amount
+
+		sl = append(sl, clinicTriagePatientID, clinicExaminationID, "'"+orderSn+"'", strconv.Itoa(index), times, "'"+organ+"'", personnelID)
+		sm = append(sm, clinicTriagePatientID, "4", clinicExaminationID, "'"+orderSn+"'", strconv.Itoa(index), "'"+name+"'", strconv.FormatInt(price, 10), strconv.Itoa(amount), "'"+unitName+"'", strconv.Itoa(total), strconv.Itoa(total), personnelID)
+
+		if illustration == "" {
+			sl = append(sl, `null`)
+		} else {
+			sl = append(sl, "'"+illustration+"'")
+		}
+
+		tstr := "(" + strings.Join(sl, ",") + ")"
+		clinicExaminationValues = append(clinicExaminationValues, tstr)
+		mstr := "(" + strings.Join(sm, ",") + ")"
+		mzUnpaidOrdersValues = append(mzUnpaidOrdersValues, mstr)
+	}
+	tSetStr := strings.Join(clinicExaminationSets, ",")
+	tValueStr := strings.Join(clinicExaminationValues, ",")
+
+	mSetStr := strings.Join(mzUnpaidOrdersSets, ",")
+	mvValueStr := strings.Join(mzUnpaidOrdersValues, ",")
+
+	tx, errb := model.DB.Begin()
+	if errb != nil {
+		fmt.Println("errb ===", errb)
+		tx.Rollback()
+		ctx.JSON(iris.Map{"code": "1", "msg": errb})
+		return
+	}
+	_, errdlp := tx.Exec("delete from examination_patient where clinic_triage_patient_id=$1", clinicTriagePatientID)
+	if errdlp != nil {
+		fmt.Println("errdlp ===", errdlp)
+		tx.Rollback()
+		ctx.JSON(iris.Map{"code": "1", "msg": errdlp.Error()})
+		return
+	}
+	_, errdm := tx.Exec("delete from mz_unpaid_orders where clinic_triage_patient_id=$1 and charge_project_type_id=4", clinicTriagePatientID)
+	if errdm != nil {
+		fmt.Println("errdm ===", errdm)
+		tx.Rollback()
+		ctx.JSON(iris.Map{"code": "1", "msg": errdm.Error()})
+		return
+	}
+
+	inserttSQL := "insert into examination_patient (" + tSetStr + ") values " + tValueStr
+	fmt.Println("inserttSQL===", inserttSQL)
+
+	_, errt := tx.Exec(inserttSQL)
+	if errt != nil {
+		fmt.Println("errt ===", errt)
+		tx.Rollback()
+		ctx.JSON(iris.Map{"code": "1", "msg": errt.Error()})
+		return
+	}
+
+	insertmSQL := "insert into mz_unpaid_orders (" + mSetStr + ") values " + mvValueStr
+	fmt.Println("insertmSQL===", insertmSQL)
+
+	_, errm := tx.Exec(insertmSQL)
+	if errm != nil {
+		fmt.Println("errm ===", errm)
+		tx.Rollback()
+		ctx.JSON(iris.Map{"code": "1", "msg": "请检查是否漏填"})
+		return
+	}
+	errc := tx.Commit()
+	if errc != nil {
+		tx.Rollback()
+		ctx.JSON(iris.Map{"code": "-1", "msg": errc.Error()})
+		return
+	}
+
+	ctx.JSON(iris.Map{"code": "200", "data": nil})
+}
+
+//ExaminationPatientGet 获取检查
+func ExaminationPatientGet(ctx iris.Context) {
+	clinicTriagePatientID := ctx.PostValue("clinic_triage_patient_id")
+	if clinicTriagePatientID == "" {
+		ctx.JSON(iris.Map{"code": "1", "msg": "缺少参数"})
+		return
+	}
+
+	rows, err := model.DB.Queryx(`select ep.*, e.name from examination_patient ep left join clinic_examination ce on ep.clinic_examination_id = ce.id 
+		join examination e on ce.examination_id = e.id
+		where ep.clinic_triage_patient_id = $1`, clinicTriagePatientID)
+
+	if err != nil {
+		ctx.JSON(iris.Map{"code": "-1", "msg": err.Error()})
+	}
+
+	result := FormatSQLRowsToMapArray(rows)
+	ctx.JSON(iris.Map{"code": "200", "data": result})
 }
