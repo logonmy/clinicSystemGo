cfe23550f592ffef59da16fa0c8f96e11ea5c17d
田先耀 <xianyao9958@163.com>
2018-05-26 17:10:32 +0800

    修改

--- controller/drug.go
+++ controller/drug.go
@@ -66,7 +66,7 @@ func DrugAdd(ctx iris.Context) {
 
 	insertSQL := `insert into drug (` + setStr + `) values ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,
 		$16,$17,$18,$19) RETURNING id;`
-	clinicDrugSQL := `insert into clinic_drug (` + clinicDrugSetStr + `) values ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)`)
+	clinicDrugSQL := `insert into clinic_drug (` + clinicDrugSetStr + `) values ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)`
 
 	tx, err := model.DB.Begin()
 
