54d5cddeb6ce7bb4de0af0eaa94edd4d0b51ea3b
ruan <894159898@qq.com>
2018-08-06 17:40:36 +0800

    se

--- controller/charge_traige_patients.go
+++ controller/charge_traige_patients.go
@@ -156,3 +156,70 @@ func GetPaidTraigePatients(ctx iris.Context) {
 	results := FormatSQLRowsToMapArray(rows)
 	ctx.JSON(iris.Map{"code": "200", "data": results, "page_info": pageInfo})
 }
+
+// GetRefundTraigePatients 获取已退费的分诊记录
+func GetRefundTraigePatients(ctx iris.Context) {
+	keyword := ctx.PostValue("keyword")
+	clinicID := ctx.PostValue("clinic_id")
+	offset := ctx.PostValue("offset")
+	limit := ctx.PostValue("limit")
+	startDate := ctx.PostValue("start_date")
+	endDate := ctx.PostValue("end_date")
+	if clinicID == "" {
+		ctx.JSON(iris.Map{"code": "-1", "msg": "缺少参数"})
+		return
+	}
+	if offset == "" {
+		offset = "0"
+	}
+	if limit == "" {
+		limit = "10"
+	}
+
+	if startDate == "" || endDate == "" {
+		ctx.JSON(iris.Map{"code": "-1", "msg": "请输入正确的时间范围"})
+		return
+	}
+	_, err := strconv.Atoi(offset)
+	if err != nil {
+		ctx.JSON(iris.Map{"code": "-1", "msg": "offset 必须为数字"})
+		return
+	}
+	_, err = strconv.Atoi(limit)
+	if err != nil {
+		ctx.JSON(iris.Map{"code": "-1", "msg": "limit 必须为数字"})
+		return
+	}
+
+	sql := ` from mz_refund_record mrr 
+	left join mz_paid_record mpr on mpr.id = mz_paid_record_id  
+	left join clinic_triage_patient ctp on mpr.clinic_triage_patient_id = ctp.id 
+	left join clinic_patient cp on cp.id = ctp.clinic_patient_id 
+	left join personnel doc on doc.id = ctp.doctor_id 
+	left join department d on d.id = ctp.department_id  
+	left join patient p on p.id = cp.patient_id 
+	left join personnel triage_personnel on triage_personnel.id = mrr.operation_id 
+	where cp.clinic_id=$1 AND mrr.created_time BETWEEN $2 and $3 AND (p.name ~$4 OR p.cert_no ~$4 OR p.phone ~$4) `
+
+	countsql := `select count(*) as total` + sql
+	querysql := `select 
+	triage_personnel.name as refund_people,
+	mrr.created_time,
+	mrr.refund_money,
+	p.name as patient_name,
+	p.birthday,
+	p.sex,
+	cp.patient_id,
+	doc.name as doctor_name,
+	d.name as department_name ` + sql + `order by mrr.updated_time DESC offset $5 limit $6`
+
+	total := model.DB.QueryRowx(countsql, clinicID, startDate, endDate, keyword)
+
+	pageInfo := FormatSQLRowToMap(total)
+	pageInfo["offset"] = offset
+	pageInfo["limit"] = limit
+
+	rows, _ := model.DB.Queryx(querysql, clinicID, startDate, endDate, keyword, offset, limit)
+	results := FormatSQLRowsToMapArray(rows)
+	ctx.JSON(iris.Map{"code": "200", "data": results, "page_info": pageInfo})
+}
