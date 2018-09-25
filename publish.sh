461d7d0c86482106ac8de9a5a40a0c560522bcdf
田先耀 <xianyao9958@163.com>
2018-09-25 09:23:04 +0800

    修改

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go  
-ssh root@47.93.206.157 "cd /root/projects/clinicSystemGo;git pull"
-scp main root@47.93.206.157:/root/projects/
-ssh root@47.93.206.157 "supervisorctl stop go-server"
-ssh root@47.93.206.157 "mv /root/projects/main /root/projects/clinicSystemGo/main"
-ssh root@47.93.206.157 "supervisorctl start go-server"
+ssh root@47.92.128.90 "cd /root/projects/clinicSystemGo;git pull"
+scp main root@47.92.128.90:/root/projects/
+ssh root@47.92.128.90 "supervisorctl stop go-server"
+ssh root@47.92.128.90 "mv /root/projects/main /root/projects/clinicSystemGo/main"
+ssh root@47.92.128.90 "supervisorctl start go-server"
